8fc1a7673643fce9dd13f8526f0e0af570832b29
Dimash1024 <116518971+Dimash1024@users.noreply.github.com>
2022-12-19 16:00:36 +0600

    Add files via upload

---
+++ EEL/main.py
@@ -0,0 +1,17 @@
+# pip install eel
+# pip install eel[jinja2]
+
+
+import eel
+from random import randint
+  
+eel.init("web")  
+  
+# Exposing the random_python function to javascript
+@eel.expose    
+def random_python():
+    print("Random function running")
+    return randint(1,100)
+  
+# Start the index.html file
+eel.start("index.html")
